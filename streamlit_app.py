0f1a32853427ed194eaa246ab952af7afbf7eec0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-17 20:03:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,33 @@
 import streamlit as st
+import base64
+from openai import OpenAI
 
-st.title('🎈 App Name')
+# App title
+st.set_page_config(page_title="👀 GPT4 Vision")
+st.title('👀 GPT4 Vision')
+st.info('This app is created using the GPT4 Vision from OpenAI.')
 
-st.write('Hello world!')
+# API Credentials
+if 'OPENAI_API_KEY' in st.secrets:
+  st.success('API key already provided!', icon='✅')
+  openai.api_key = st.secrets['OPENAI_API_KEY']
+else:
+  openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+  if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+    st.warning('Please enter your credentials!', icon='⚠️')
+  else:
+    st.success('Proceed to entering your prompt message!', icon='👉')
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Initialize OpenAI client with API key
+client = OpenAI(api_key=api_key)
+
+# Upload image
+image_upload = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
+
+
+
