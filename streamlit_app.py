3bb27082a732c240d258246b6e9175b5358630b2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-21 19:32:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ from tempfile import NamedTemporaryFile
 # App title
 st.set_page_config(page_title='🎈 Streamlit App Builder', page_icon='🎈', layout='wide')
 st.title('🎈 Streamlit App Builder')
-st.info('This app builder is created using the GPT-4 with Vision (GPT-4V) from OpenAI.')
+st.info('In this app you can **show** (provide mock-up image) or **tell** (provide text prompt) how you want your Streamlit app to be built.')
+# This app builder is created using the GPT-4 with Vision (GPT-4V) from OpenAI.
 
 # CSS styling
 st.markdown("""
@@ -21,85 +22,83 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-# tabs = st.tabs(['Provide mock-up image', 'Provide text instructions'])
+tabs = st.tabs(['Show', 'Tell'])
 
-# Upload image
-image_upload = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
-
-with st.expander('Expand to edit prompt instructions'):
-  prompt_instructions = st.text_area("Prompt instructions",
-                          "You are an experienced Python developer who can build amazing Streamlit apps.\n"
-                          "You will be given a mock-up image of a Streamlit app for which you will convert it to a Streamlit app by generating the Python code.\n"
-                        )
-
-# Function to encode the image
-def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-
-
-if image_upload:
-  st.image(image_upload, use_column_width=True)
-
-  # base64_image = encode_image(image_upload)
-  # st.write(base64_image)
-
-  bytes_data = image_upload.read()
-  with NamedTemporaryFile(delete=False) as tmp:
-    tmp.write(bytes_data)
-  
+with tabs[0]:
+    # Upload image
+    image_upload = st.file_uploader('Upload an image', type=['png', 'jpg', 'jpeg'])
+    
+    with st.expander('Expand to edit prompt instructions'):
+      prompt_instructions = st.text_area("Prompt instructions",
+                              "You are an experienced Python developer who can build amazing Streamlit apps.\n"
+                              "You will be given a mock-up image of a Streamlit app for which you will convert it to a Streamlit app by generating the Python code.\n"
+                            )
+    
+    # Function to encode the image
+    def encode_image(image_path):
+      with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
     
     
-
-# Start LLM process
-start_button = st.button('Build')
-
-# Initialize OpenAI client with API key
-api_key = st.secrets['OPENAI_API_KEY']
-client = OpenAI(api_key=api_key)
-
-if image_upload is not None and api_key and start_button:
-# if image_upload is not None and openai.api_key and start_button:
-  with st.spinner('Processing ...'):
-    base64_image = encode_image(tmp.name)
+    if image_upload:
+      st.image(image_upload, use_column_width=True)
     
-    messages = [
-            {
-                'role': 'user',
-                'content': [
-                    {'type': 'text', 'text': prompt_instructions},
-                    {
-                        'type': 'image_url', 'image_url': f'data:image/jpeg;base64,{base64_image}',
-                    },
-                ],
-            }
-        ]
-
-    try:
-      # Response generation
-      full_response = ''
-      message_placeholder = st.empty()
-          
-      for completion in client.chat.completions.create(
-        model='gpt-4-vision-preview', messages=messages, 
-        max_tokens=1280, stream=True):
-                  
-          if completion.choices[0].delta.content is not None:
-            full_response += completion.choices[0].delta.content
-            message_placeholder.markdown(full_response + '▌')
-                  
-      message_placeholder.markdown(full_response)
-
-      # Clear results
-      if st.button('Clear'):
-        os.remove(tmp.name)
+      # base64_image = encode_image(image_upload)
+      # st.write(base64_image)
     
-    except Exception as e:
-      st.error(f'An error occurred: {e}')
+      bytes_data = image_upload.read()
+      with NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(bytes_data)
       
-else:
-  if not image_upload and start_button:
-    st.warning('Please upload your mock-up image.')
-  if not api_key:
-    st.warning('Please provide your OpenAI API key.')
+    # Start LLM process
+    start_button = st.button('Build')
+    
+    # Initialize OpenAI client with API key
+    api_key = st.secrets['OPENAI_API_KEY']
+    client = OpenAI(api_key=api_key)
+    
+    if image_upload is not None and api_key and start_button:
+    # if image_upload is not None and openai.api_key and start_button:
+      with st.spinner('Processing ...'):
+        base64_image = encode_image(tmp.name)
+        
+        messages = [
+                {
+                    'role': 'user',
+                    'content': [
+                        {'type': 'text', 'text': prompt_instructions},
+                        {
+                            'type': 'image_url', 'image_url': f'data:image/jpeg;base64,{base64_image}',
+                        },
+                    ],
+                }
+            ]
+    
+        try:
+          # Response generation
+          full_response = ''
+          message_placeholder = st.empty()
+              
+          for completion in client.chat.completions.create(
+            model='gpt-4-vision-preview', messages=messages, 
+            max_tokens=1280, stream=True):
+                      
+              if completion.choices[0].delta.content is not None:
+                full_response += completion.choices[0].delta.content
+                message_placeholder.markdown(full_response + '▌')
+                      
+          message_placeholder.markdown(full_response)
+    
+          # Clear results
+          if st.button('Clear'):
+            os.remove(tmp.name)
+        
+        except Exception as e:
+          st.error(f'An error occurred: {e}')
+          
+    else:
+      if not image_upload and start_button:
+        st.warning('Please upload your mock-up image.')
+      if not api_key:
+        st.warning('Please provide your OpenAI API key.')
 
