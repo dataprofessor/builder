6bf50d42b8df975e28830ada3ff7c78b2da34a72
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-22 00:27:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ with tabs[0]:
                         ],
                 #captions=["A cat", "Another cat", "Oh look, a cat!", "Guess what, a cat..."],
                 )
-    if example_img is not None:
+    if img:
         img
     
     with st.expander('Expand to edit prompt instructions'):
