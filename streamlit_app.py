334c80dafd9fe8bf6407d81e162d1acf921d88aa
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-06-27 13:31:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import base64
 import time
 import openai
 from openai import OpenAI
+from tempfile import NamedTemporaryFile
 from streamlit_image_select import image_select
 from mockup_code import mockup_1, mockup_2
 
